30f3d0babe08003ad690673d8e119ca07f3fc90b
Parsankamusa <parsankamusa@gmail.com>
2022-09-26 07:48:23 -0700

    completed task

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    """Returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
+    ireturn (len(sentence), sentence[0])
