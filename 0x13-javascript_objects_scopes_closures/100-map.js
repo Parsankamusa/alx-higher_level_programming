a8cdbdf94a0b85d6e05537285564af10f7395940
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2022-12-20 19:55:02 +0300

    Delete 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// script that imports an array and computes a new array.
-
-const originalList = require('./100-data').list;
-console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-    return (e * index);
-});
-console.log(mappedList);
